9f69a546211209642d4c40472a594ad40b1dbebd
Clon01 <clon01@users.noreply.github.com>
2018-05-25 17:27:03 +0200

    Added Movie class Added MovieScrapper class Testing lines on start.py

---
+++ data/MovieScrapper.py
@@ -0,0 +1,107 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from models.movie import Movie
+
+
+class MovieScrapper:
+    """Returns movies from the website"""
+
+    def __init__(self, url: str):
+        """Creates a movie scrapper object"""
+        self.soup = self.get_movie_soup(url)
+
+    def get_title(self, node):
+        """
+        Returns the title from the currently selected movie in the BeautifulSoup node
+        :param node: A soup object positioned in the movie root
+        :return: str: The movie title
+        """
+        return node.h3.a.text.trim()
+
+    def get_director(self, node):
+        """
+        Returns the director from the currently selected movie in the BeautifulSoup node
+        :param node: A soup object positioned in the movie root
+        :return: str: The movie director
+        """
+        return node.h6.text.trim()
+
+    def get_section(self, node) -> str:
+        """
+        Returns the section from the currently selected movie in the BeautifulSoup node
+        :rtype: str
+        :param node: A soup object positioned in the movie root
+        :return: str: The movie section
+        """
+        return node.p.text.trim()
+
+    def get_link(self, node) -> str:
+        """
+        Returns the link from the currently selected movie in the BeautifulSoup node
+        :rtype: str
+        :param node: A soup object positioned in the movie root
+        :return: str: The movie section
+        """
+        return node.h3.a
+
+    def get_movie_soup(self, link):
+        """
+        Returns a BeautifulSoup object for the movie sub page
+        :param link: str: Link to the movie sub page
+        :return: A soup object containing the full sub page
+        """
+        return BeautifulSoup(requests.get(link), "html.parser")
+
+    def get_synopse(self, soup):
+        """
+        Returns the synopse from the movie page in the BeautifulSoup node
+        :param soup: A soup object containing the movie page
+        :return: str: The movie synopse
+        """
+        return soup.find("div", {"class": "section_sinopsi"}).p.text.trim()
+
+    def get_duration(self, soup):
+        """
+        Returns the duration from the movie page in the BeautifulSoup node
+        :param soup: A soup object containing the movie page
+        :return: str: The movie duration
+        """
+        return soup.find("div", {"class": "section_fitxa_artistica"}).p.strong.text.trim()
+
+    def get_movie(self, node):
+        """
+        Returns a Movie object with all the data found in the bs4 node
+        :param node: A soup object positioned in the movie root
+        :return: The Movie object
+        """
+        subpage = self.get_movie_soup(self.get_link(node))
+        return Movie(title=self.get_title(node), director=self.get_director(node), section=self.get_section(node),
+                     synopse=self.get_synopse(subpage), duration=self.get_duration(subpage))
+
+    def is_movie_in_section(self, node, exclusion: str):
+        """
+        Returns a list of movies excluding all movies in the matching sections
+        :param node: A soup object positioned in the movie root
+        :param exclusion: str: Regular expression. All movies with a section matching this string will not be returned
+        :return: A collection of Movie objects
+        """
+        re.match(exclusion, self.get_section(node))
+        
+    def slice_soup_by_movies(self):
+        """
+        Returns a list with all movie nodes
+        :return: A collection of soup nodes containing movie info
+        """
+        for node in self.soup.findAll("div", {"class": "right-banner-bottom"}):
+            yield node
+
+    def get_movies_by_section(self, exclusion: str):
+        """
+        Returns a list of movies excluding all movies in the matching sections
+        :param exclusion: str: Regular expression. All movies with a section matching this string will not be returned
+        :return: A collection of Movie objects
+        """
+        for node in self.slice_soup_by_movies():
+            if not self.is_movie_in_section(node, exclusion):
+                yield self.get_movie(node)
---
+++ start.py
@@ -0,0 +1,8 @@
+from data.MovieScrapper import MovieScrapper
+
+if __name__ == '__main__':
+    url = "http://www.sitgesfilmfestival.com/cat/programa/pel_licules"
+    scrp = MovieScrapper(url)
+    exclusion = "urts|Brigadoon|Espai|Serial|SGAE|Petit|ort"
+    movies = scrp.get_movies_by_section(exclusion)
+
