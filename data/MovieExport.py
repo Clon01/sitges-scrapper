c0f24ee14da490c6998cc658bbf6ac1612641d22
Clon01 <github@frikimon.com>
2021-09-15 17:51:15 +0200

    First draft to multi-export

--- data/MovieExport.py
+++ data/MovieExport.py
@@ -1,4 +1,38 @@
 from lib.html import HTML
+from enum import Enum, auto
+from csv import DictWriter
+
+
+class Formats(Enum):
+    HTML = auto()
+    CSV = auto()
+
+
+class ExportFactory:
+
+    def __init__(self):
+        self._factory = dict()
+        self._factory[Formats.HTML.name] = Movie2HTML
+        self._factory[Formats.CSV.name] = Movie2CSV
+        self._exporter = list()
+        self._movies = list()
+
+    def add_output(self, file_format:Formats, file_name):
+        self._exporter.append(
+            self._factory[file_format.name](file_name)
+        )
+
+    def add_movie(self, movie):
+        self._movies.append(movie)
+
+    def save_to_file(self):
+        for exporter in self._exporter:
+            for movie in self._movies:
+                exporter.add_movie(movie)
+            exporter.save_to_file()
+
+    def count(self):
+        return len(self._movies)
 
 
 class MovieExport:
@@ -57,3 +91,71 @@ class MovieExport:
     #     :rtype: int
     #     """
     #     return self.counter
+
+
+class Movie2HTML:
+
+    def __init__(self, file_name):
+        # Create a new empty html object
+        self.output = HTML('html')
+        self.file_name = file_name
+
+    def add_movie(self, movie):
+        """
+        Add a movie object to the output html
+        :param movie: A Movie object
+        :return: None
+        """
+        # Create a new table on the html
+        t = self.output.body.table(border="1")
+        # Add a first row
+        r = t.tr
+        # Write title and director on the first row
+        # Also define 20/80% ratio for 2 cells
+        r.td(movie.director, width="20%")
+        r.td(movie.title)
+        # Add a secod row
+        r2 = t.tr
+        # Write section and duration on the second row
+        r2.td(movie.section)
+        r2.td(movie.duration)
+        # Add a third row
+        r3 = t.tr
+        # Start with an empty row then write the synopse
+        r3.td()
+        r3.td(movie.synopse)
+        # Add a line break after the table
+        self.output.body.br()
+        # This function returns nothing
+
+    def save_to_file(self):
+        """
+        Saves the added film to an html file
+        :return: None
+        """
+        # Open the filename to be re-written
+        with open(self.file_name, 'w+', encoding='utf-8') as outfile:
+            # write the content of the html output to the file
+            outfile.write(self.output.__str__())
+
+
+class Movie2CSV:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self._movies = list()
+
+    def add_movie(self, movie):
+        self._movies.append(
+            {
+                "Title": movie.title,
+                "Year": movie.duration[-4:],
+                "Directors": movie.director.replace('&', ',')
+            }
+        )
+
+    def save_to_file(self):
+        with open(self.file_name, 'w+') as csv_file:
+            csv_writer = DictWriter(csv_file, self._movies[0].keys(), lineterminator='\n')
+            csv_writer.writeheader()
+            csv_writer.writerows(self._movies)
--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 #  import pandas
 from data.MovieScrapper import MovieScrapper
-from data.MovieExport import MovieExport
+from data.MovieExport import ExportFactory, Formats
 from data.Settings import Settings
 
 if __name__ == '__main__':
@@ -10,16 +10,19 @@ if __name__ == '__main__':
     scrap = MovieScrapper(my.settings["URL"], my.settings.get("Params"))
     # Check if the content of the page has changed
     if not my.check_hash(scrap.get_hash()):
-        # Create a new MovieExport object
-        me = MovieExport()
+        # Create a new ExportFactory object
+        ef = ExportFactory()
+        # Add each export format to the factory
+        for export in my.settings["Exports"]:
+            ef.add_output(Formats[export["Format"]], export["Filename"])
         # Loop all the movies from the MovieScrapper, excluding banned sections in the settings file
         for m in scrap.get_movies_by_section(my.banned_sections()):
             # Add each movie to the export html
-            me.add_movie(m)
-        # Save export html to the file from settings
-        me.save_to_file(my.settings["ExportFile"])
+            ef.add_movie(m)
+        # Save export to all the export configs in the factory
+        ef.save_to_file()
         # Print movie total
-        print("Total movie count: {}".format(me.count))
+        print("Total movie count: {}".format(ef.count()))
         # Save new hash in setting.json
         my.save_settings()
     else:
